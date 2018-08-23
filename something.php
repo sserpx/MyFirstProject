6407753ee31c455123e58235f3fcf59ba7eaddbe
sserpx <paul.janku@gmail.com>
2018-08-23 10:47:26 +0200

    Bad Motherfucker line

--- something.php
+++ something.php
@@ -1,3 +1,11 @@
 <?php
 
 echo 'Now, take a look in there and find my wallet.';
+
+echo '<br/><br/>';
+
+echo 'Which one is it?';
+
+echo '<br/><br/>';
+
+echo 'The one that says Bad Motherfucker.';
